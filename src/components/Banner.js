985b2c8f9074051e8e257c406c2e0c57e67fc964
darkrider88 <shashwatpandey88@gmail.com>
2025-04-13 21:11:11 +0530

    changes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
     <section id="home" className="banner">
       <div className="banner-content">
         <h1>Shashwat Pandey</h1>
-        <p className="subtitle">I'm a passionate softwware engineer creating efficient and scalable solutions</p>
+        <p className="subtitle">I'm a passionate software engineer creating efficient and scalable solutions</p>
         <div className="cta-buttons">
           <a href="https://drive.google.com/file/d/1RYgF1OF7OyJTRnUi7Bi5vHiGvIXB_pJw/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="cta-button primary">Resume</a>
           <a href="#contact" className="cta-button secondary">Contact Me</a>
